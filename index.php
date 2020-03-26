c3f1eef50cca66ae192fe279e2a9b1fbf32354aa
djfox <smartgit@djfox.ru>
2020-03-26 12:34:33 +0300

    Создан Index файл

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+ini_set('display_errors','On');
+include "include/Func.php";
+include "include/CRM.php";
+
+$crm = new CRM();
+$page = $crm->GetPage();
+$menu = $crm->GetMenu( $page );
+
+include "include/pages/header.php";
+echo $menu;
+include "include/pages/$page.php";
+include "inc/footer.php";
+
+?>
